bf0592978152a3ba3639e991608522949da712c0
Mariusz Bielicki <mbielicki@wp.pl>
2021-12-17 15:42:55 +0100

    Final version 2

--- submenu.php
+++ submenu.php
@@ -20,7 +20,7 @@ session_start();
 	
 		<p><input type="radio" id="name" name="submenu" value="name" checked><label for="name">Change your name</label></p>
 		<p><input type="radio" id="surname" name="submenu" value="surname">Change your surname</label></p>
-		<p><input type="radio" id="pin" name="submenu" value="pin"><label for="pin">Change PIN</label></p>
+		<p><input type="radio" id="pin" name="submenu" value="pin"><label for="pin">Change your PIN</label></p>
 		<p><input type="radio" id="check" name="submenu" value="check"><label for="check">Check your private data</label></p>
 		<p><input type="radio" id="exit" name="submenu" value="exit"><label for="exit">Exit</label></p>
 		<input type="submit" value="OK">
